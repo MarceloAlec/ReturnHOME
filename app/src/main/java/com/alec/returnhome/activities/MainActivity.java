ef44d95b521ca391c26f0d1c49e9eb83c19df03d
Marcelo Carvajal <marcelo_alec@hotmail.com>
2021-10-07 16:06:10 -0500

    Creo evento click a los botones del main activity

--- app/src/main/java/com/alec/returnhome/activities/MainActivity.java
+++ app/src/main/java/com/alec/returnhome/activities/MainActivity.java
@@ -2,15 +2,47 @@ package com.alec.returnhome.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.alec.returnhome.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //PARA REFERENCIAR LOS BOTONES
+    Button mButtonLogin;
+    Button mButtonRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //SE OBTIENE LA REFERENCIA DE LOS BOTONES
+        mButtonLogin = findViewById(R.id.btnLogin);
+        mButtonRegister = findViewById(R.id.btnRegister);
+
+        //AÑADE EL EVENTO CLICK A LOS BOTONES
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //SE CREA UNA INTENCION PARA PASAR DE UNA ACTIVIDAD A OTRA
+                //TOMA COMO ARGUMENTOS EL CONTEXTO ACTUAL Y LA ACTIVIDAD A DIRIGIRSE
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
