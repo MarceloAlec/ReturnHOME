c9ab66d571b7a6cf5711327d5bf125530d29b3a0
Marcelo Carvajal <marcelo_alec@hotmail.com>
2021-10-08 16:54:50 -0500

    Creo archivo php para el registro de usuarios

---
+++ ws/register.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'connection.php' ;
+ 
+ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ 
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $pass = $_POST['pass'];
+
+ $sqlQuery = "insert into tblUser(name,email,pass) values ('$name','$email','$pass')";
+ 
+ if(mysqli_query($con,$sqlQuery)){
+ echo 'Datos enviados con éxito';
+ }
+ else{
+ echo 'Inténtalo de nuevo';
+ }
+ mysqli_close($con);
+?>
