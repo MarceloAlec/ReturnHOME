47733aa325b458d70234eb57eea1a3c17e9e3619
Marcelo Carvajal <marcelo_alec@hotmail.com>
2021-10-12 12:58:47 -0500

    Cambio la estructura de los archivos para el ws

--- api.returnhome.com/v1/register.php
+++ api.returnhome.com/v1/register.php
@@ -13,18 +13,38 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     //MEDIANTE PREPARE() SE CREA UNA INSTANCIA DE LA CLASE PDO STATEMENT
     //PARA PASAR LOS VALORES O EJECUTAR SENTENCIAS
     $stmt = $pdo->prepare($sql);
+
+
+    $name="Marcelo";
+    $email="m@gmail.com";
+    $pass="123456";
     
     //CON BINDVALUE SE ENLAZA EL VALOR DE LA VARIABLE
-    $stmt->bindValue(':name',$_POST['name']);
-    $stmt->bindValue(':email',$_POST['email']);
-    $stmt->bindValue(':pass',$_POST['pass']);
+    $stmt->bindValue(':name',$name);
+    $stmt->bindValue(':email',$email);
+    $stmt->bindValue(':pass',$pass);
     //EJECUTA LA SENTENCIA SQL, ENVIA LOS DATOS A LA BASE
     $stmt->execute();
-    $idPost = $pdo->lastInsertId();
+    /* $idPost = $pdo->lastInsertId();
     if($idPost){
         $val = array("id" => $idPost);
         echo json_encode($val);
-    }
+    } */
+
+   /*  $versiones = array(
+        "versiones_android" => array(
+            array(
+                "froyo" => "2.2"
+            ),
+            array(
+                "gingerbread" => "2.3"
+            )
+        )
+    ); */
+
+    $versiones = array("estado"=>"201");
+    header('Content-Type: application/json');
+    echo json_encode($versiones);
     
 }
 
