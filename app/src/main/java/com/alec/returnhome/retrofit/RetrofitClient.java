f02c1a46e771b3cf7c1fb811417ac868b36f1f93
Marcelo Carvajal <marcelo_alec@hotmail.com>
2021-10-18 18:35:10 -0500

    Cambio gson por scalars
    Ya que el objeto json retornado por parte del webservice tendra diversos parametros conviene que sea obtenido como un string por tal razon se agrega scalars en lugar de gson

--- app/src/main/java/com/alec/returnhome/retrofit/RetrofitClient.java
+++ app/src/main/java/com/alec/returnhome/retrofit/RetrofitClient.java
@@ -2,17 +2,18 @@ package com.alec.returnhome.retrofit;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 
 public class RetrofitClient {
 
     private static Retrofit retrofit;
 
-    //URL DONDE SE REALIZA LA PETICION
+
     public static Retrofit getClient(String url){
         retrofit = new Retrofit.Builder()
                             .baseUrl(url)
-                            .addConverterFactory(GsonConverterFactory.create())
+                            .addConverterFactory(ScalarsConverterFactory.create())
                             .build();
 
         return retrofit;
